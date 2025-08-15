289ddf426c2cfce4e20ed42eaa82ecf833748de4
HIMANSHUHM402 <avimauryaofficial50@gmail.com>
2025-08-15 07:53:16 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "your api id"))
-API_HASH = environ.get("API_HASH", "api hash")
+API_ID = int(environ.get("API_ID", "21589942"))
+API_HASH = environ.get("API_HASH", "f99db4b15e3179c794a058b21b4ad8da")
 BOT_TOKEN = environ.get("BOT_TOKEN", " bot token ")
 
-OWNER = int(environ.get("OWNER", "owner id "))
+OWNER = int(environ.get("OWNER", "8034639315"))
 CREDIT = environ.get("CREDIT", " apka name")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
